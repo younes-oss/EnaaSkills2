6b1ae6f0377288ba45f63e6a959d0f34d70b92ec
younes-oss <aitchattouyounes@gmail.com>
2025-07-09 17:04:10 +0200

    just try

--- competence-service/src/main/java/com/enaa_skills/competence_service/repository/SousCompetenceRepository.java
+++
@@ -1,7 +0,0 @@
-package org.example.enaaskills.repository;
-
-import org.example.enaaskills.model.SousCompetence;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SousCompetenceRepository extends JpaRepository<SousCompetence, Long> {
-} 
