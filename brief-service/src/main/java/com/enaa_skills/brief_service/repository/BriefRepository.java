03b19263c8ba1c3bc678dfb5e5298e2f92b526a3
younes-oss <aitchattouyounes@gmail.com>
2025-07-18 16:07:55 +0200

    ajout des depndances et la configuration

--- brief-service/src/main/java/com/enaa_skills/brief_service/repository/BriefRepository.java
+++ brief-service/src/main/java/com/enaa_skills/brief_service/repository/BriefRepository.java
@@ -1,9 +1,12 @@
 package com.enaa_skills.brief_service.repository;
 
 import com.enaa_skills.brief_service.Model.Brief;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BriefRepository extends CrudRepository<Brief,Long> {
+
+    
 }
